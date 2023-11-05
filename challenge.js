e4975cbda536ed88f68bea8d55339875b5c52a23
NIMCOO <nimco77001@gmail.com>
2023-11-05 16:11:12 +0000

    well done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+// calculate BMI =weight/(heigh*height)
+
+function BMI(weight,height){
+    return (weight,height)
+     console. log("Ali was bigger than Mohammed in length and width")
+    }   
+    BMI(Ali,48kg ,60)
+    
+    let mohamedweighheight =(40 kg ,60 m)
+    if (Aliweightheight > mohamedweighheight)
+    console. log("mohamed is less than Mohammed in length and width" )
+    
+    
+    
+    
+    
+    
+    
+    
+    
